c5626a1f311296129990c1c5675bfd74dd8534a3
Mark <markhskinner@gmail.com>
2021-03-18 23:43:59 -0600

    fixed various bugs

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var sheetID = '16y1ZUR_HXVSG-OPxCPDVQ-5WpPFl1dv4Rn1Uv0x0QkI';
 
 var gotData = false;
 var data = []; //local copy of spreadsheet that's more readable
-
+var layouts = [];
 
 $(document).ready( function() {
     var now = new Date();
@@ -42,16 +42,12 @@ var layout = {
     b: 100,
     t: 50,
     pad: 4
-  },
-  title: {
-    text:'Plot Title',
   }
 }
 var config = {
   responsive: true,
   displayModeBar: true
 };
-
 //https://docs.google.com/spreadsheets/d/16y1ZUR_HXVSG-OPxCPDVQ-5WpPFl1dv4Rn1Uv0x0QkI
 
 //json callback
@@ -69,6 +65,7 @@ function getSheet(pageIndex = 1) {
   if(pageIndex < 1){
     document.getElementById('waiting').style.display = 'none';
     document.getElementById('error').style.display = 'inline';
+    setLabels(false);
   }
   if(pageIndex <= 35){ //limit to 35 pages of history
     $.getScript(`https://spreadsheets.google.com/feeds/cells/${sheetID}/${pageIndex}/public/values?alt=json-in-script&callback=doData`)
@@ -81,16 +78,19 @@ function getSheet(pageIndex = 1) {
       console.log('could not get GS', jqxhr, settings, exception);
       document.getElementById('waiting').style.display = 'none';
       document.getElementById('error').style.display = 'inline';
+      setLabels(false);
     });
   }else{
     document.getElementById('waiting').style.display = 'none';
     document.getElementById('error').style.display = 'inline';
+    setLabels(false);
   }
 }
 
 function dateChangeCallback(date){
   getSheet(datediff(parseDate(date)));
     document.getElementById('waiting').style.display='inline';
+    setLabels(false);
     document.getElementById('error').style.display = 'none';
   Plotly.purge('ROVgraph');
   Plotly.purge('Lightsgraph');
@@ -99,7 +99,6 @@ function dateChangeCallback(date){
   Plotly.purge('Atmosphere');
   Plotly.purge('Fan');
 }
-
 function parseDate(str) {
   var b = str.split(/\D/);
   return new Date(b[0], --b[1], b[2]);
@@ -109,9 +108,15 @@ function datediff(first) {
     return Math.round((new Date().getTime() -first)/(1000*60*60*24));
 }
 
+function setLabels(state){
+  const c = document.getElementsByClassName('graphTitle');
+  for(let i = 0; i < c.length; i++) c[i].style.display = state?'block':'none';
+}
+
 var divs = [];
 function updateGraphs(){
   document.getElementById('waiting').style.display='none';
+  setLabels(true);
   var mdata = [];
   let time = getFormatedTime(data);
   var i = 3; //0 is empty, and 1 is timestamp and 2 is pi temp
@@ -124,7 +129,6 @@ function updateGraphs(){
     });
   }
   divs[0] = document.getElementById('ROVgraph');
-  layout.title.text = 'ROV';
   Plotly.plot(divs[0], mdata, layout, config);
 
   mdata = [];
@@ -137,7 +141,6 @@ function updateGraphs(){
     });
   }
   divs[1] = document.getElementById('Lightsgraph');
-  layout.title.text = 'Lights';
   Plotly.plot(divs[1], mdata, layout, config);
 
   mdata = [];
@@ -150,7 +153,6 @@ function updateGraphs(){
     });
   }
   divs[2] = document.getElementById('Hydrophone');
-  layout.title.text = 'Hydrophone';
   Plotly.plot(divs[2], mdata, layout, config);
 
   mdata = [];
@@ -163,7 +165,6 @@ function updateGraphs(){
     });
   }
   divs[3] = document.getElementById('Camera');
-  layout.title.text = 'Camera';
   Plotly.plot(divs[3], mdata, layout, config);
 
   mdata = [];
@@ -176,7 +177,6 @@ function updateGraphs(){
     });
   }
   divs[4] = document.getElementById('Atmosphere');
-  layout.title.text = 'Atmosphere';
   Plotly.plot(divs[4], mdata, layout, config);
 
   mdata = [];
@@ -189,13 +189,12 @@ function updateGraphs(){
     });
   }
   divs[5] = document.getElementById('Fan');
-  layout.title.text = 'Fan';
   Plotly.plot(divs[5], mdata, layout, config);
 
   divs.forEach((div,i) => {
       div.on("plotly_relayout", function(ed) {
-        console.log('relayout others');
-        divs.forEach(div2 => {
+        // console.log(div.layout);
+        divs.forEach((div2,j) => {
           if(div2 != div) Plotly.update(div2,{},div.layout);
         });
       });
